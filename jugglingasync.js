a4f6c654db0837dc82c0a052352cbe228eac0e2b
U-Lovelace\Hobbes <Hobbes@Lovelace.(none)>
2014-11-04 20:59:12 -1000

    changed jugglingasync to not use for loops

--- jugglingasync.js
+++ jugglingasync.js
@@ -11,26 +11,35 @@ function readoutstrings() {
 	});
 }
 
-function grabstrings(url, ii) {
-	http.get(url, function (response) {
-		var content = '';
-		response.setEncoding('utf8');
-		response.on('error', function (err) {
-			console.log('Error: ', err);
-		});
-		response.on('data', function (data) {
-			content += data;
-		});
-		response.on('end', function () {
-      fullCharList[ii] = content;
-			countdown -= 1;
+function getUrlContent(url, callback) {
+  http.get(url, function (response) {
+    var content = '';
+    response.setEncoding('utf8');
+    response.on('error', function (err) {
+      return callback(err);
+    });
+    response.on('data', function (data) {
+      content += data;
+    });
+    response.on('end', function () {
+      return callback(null,  content);
+    });
+  });
+}
+
+function grabstrings(url, index, array) {
+  if (index > 1) {
+    getUrlContent(url, function (err, content) {
+      if (err) {
+        return console.log(err);
+      }
+      fullCharList[index - 2] = content;
+      countdown -= 1;
       if (countdown === 0) {
         readoutstrings();
       }
-		});
-	});
+    });
+  }
 }
 
-for (i = 0; i <= process.argv.length - 3; i += 1) {
-	grabstrings(process.argv[i + 2], i);
-}
+process.argv.forEach(grabstrings);
